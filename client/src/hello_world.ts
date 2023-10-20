3738584d77d790c4def6fb79982faccb3fc36e40
iccee0 <git@roeh.ch>
2023-10-20 17:04:42 +0000

    Program saving value to chain

--- client/src/hello_world.ts
+++ client/src/hello_world.ts
@@ -161,7 +161,7 @@ export async function checkProgram(): Promise<void> {
   console.log(`Using program ${programId.toBase58()}`);
 
   // Derive the address (public key) of a greeting account from the program so that it's easy to find later.
-  const GREETING_SEED = 'hello';
+  const GREETING_SEED = 'rouven';
   greetedPubkey = await PublicKey.createWithSeed(
     payer.publicKey,
     GREETING_SEED,
--- program/src/processor.rs
+++ program/src/processor.rs
@@ -1,34 +1,96 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    log::{sol_log_compute_units, sol_log_params, sol_log_slice},
     msg,
+    program_error::ProgramError,
     pubkey::Pubkey,
 };
 
-/// Instruction processor
+/// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    /// number of greetings
+    pub counter: u32,
+}
+
+// Program entrypoint's implementation
 pub fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
 ) -> ProgramResult {
-    // Log a string
-    msg!("static string");
+    msg!("Hello World Rust program entrypoint");
 
-    // Log a slice
-    sol_log_slice(instruction_data);
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
 
-    // Log a formatted message, use with caution can be expensive
-    msg!("formatted {}: {:?}", "message", instruction_data);
+    // Get the account to say hello to
+    let account = next_account_info(accounts_iter)?;
 
-    // Log a public key
-    program_id.log();
+    // The account must be owned by the program in order to modify its data
+    if account.owner != program_id {
+        msg!("Greeted account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
 
-    // Log all the program's input parameters
-    sol_log_params(accounts, instruction_data);
+    // Increment and store the number of times the account has been greeted
+    let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
+    greeting_account.counter += 1;
+    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
-    // Log the number of compute units remaining that the program can consume.
-    sol_log_compute_units();
+    msg!("Greeted {} time(s)!", greeting_account.counter);
 
     Ok(())
+}
+
+// Sanity tests
+#[cfg(test)]
+mod test {
+    use super::*;
+    use solana_program::clock::Epoch;
+    use std::mem;
+
+    #[test]
+    fn test_sanity() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
+        );
+        let instruction_data: Vec<u8> = Vec::new();
+
+        let accounts = vec![account];
+
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            0
+        );
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            1
+        );
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        assert_eq!(
+            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
+                .unwrap()
+                .counter,
+            2
+        );
+    }
 }
